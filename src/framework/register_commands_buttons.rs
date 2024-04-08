8bec00f6a912f5db638bec4cf49e1f265b743e23
AsPulse / あすぱる <contact@aspulse.dev>
2024-04-08 23:37:33 +0900

    fix #13

--- src/framework/register_commands_buttons.rs
+++ src/framework/register_commands_buttons.rs
@@ -1,6 +1,8 @@
 use crate::db::race::race;
 use crate::db::race::RaceKind::CommandInvocation;
+use crate::framework::ADMIN_GUILD_ID;
 use crate::{Context, Error};
+use log::warn;
 use poise::command;
 
 #[command(prefix_command)]
@@ -8,6 +10,10 @@ pub async fn register(ctx: Context<'_>) -> Result<(), Error> {
     if race(&ctx, CommandInvocation, ctx.id().to_string()).await? {
         return Ok(());
     };
+    if ctx.guild_id() != Some(*ADMIN_GUILD_ID) {
+        warn!("ignoring register command from non-admin guild.");
+        return Ok(());
+    }
     poise::builtins::register_application_commands_buttons(ctx).await?;
     Ok(())
 }
